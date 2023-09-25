002ab3b4ad767b78b83ba4297956cdaa1423e5dd
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2023-09-25 20:28:28 +0530

    Create sum-of-distances-in-tree.py

---
+++ sum-of-distances-in-tree.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def sumOfDistancesInTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = {}
+        for u, v in edges: 
+            graph.setdefault(u, []).append(v)
+            graph.setdefault(v, []).append(u)
+        
+        size = [0]*n
+        
+        def fn(x, par): 
+            c = s = 0
+            for xx in graph.get(x, []): 
+                if xx != par: 
+                    cc, ss = fn(xx, x)
+                    c, s = c + cc, s + ss + cc
+            size[x] = c + 1
+            return c + 1, s
+        
+        ans = [0]*n
+        ans[0] = fn(0, -1)[1]
+        
+        stack = [0]
+        while stack: 
+            x = stack.pop()
+            for xx in graph.get(x, []): 
+                if not ans[xx]: 
+                    ans[xx] = ans[x] + n - 2*size[xx]
+                    stack.append(xx)
+        return ans 
