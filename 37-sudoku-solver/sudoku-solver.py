5b9a865f4d729da31a9d948a025c82e968d3300f
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2023-12-18 11:35:26 +0530

    Time: 378 ms (42.93%) | Memory: 16.3 MB (74.11%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        
+        n = 9
+
+        def isValid(row, col, num):
+            for i in range(n):
+                if board[i][col] == num or board[row][i] == num or board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
+                    return False
+            return True
+
+        def solve(row, col):
+            if row == n:
+                return True
+            if col == n:
+                return solve(row + 1, 0)
+
+            if board[row][col] == ".":
+                for num in map(str, range(1, 10)):
+                    if isValid(row, col, num):
+                        board[row][col] = num
+                        if solve(row, col + 1):
+                            return True
+                        else:
+                            board[row][col] = "."
+                return False
+            else:
+                return solve(row, col + 1)
+
+        solve(0, 0)
