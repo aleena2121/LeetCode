3c54b71d48f7129a1552d40b47ec5561447879f1
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2024-02-01 00:43:52 +0530

    Time: 1011 ms (81.93%) | Memory: 25.1 MB (87.51%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        result = [0] * len(temperatures)
+        stack = []
+
+        for i in range(len(temperatures) - 1, -1, -1):
+            curr_temperature = temperatures[i]
+
+            while stack and curr_temperature >= temperatures[stack[-1]]:
+                stack.pop()
+
+            if stack:
+                result[i] = stack[-1] - i
+
+            stack.append(i)
+
+        return result
