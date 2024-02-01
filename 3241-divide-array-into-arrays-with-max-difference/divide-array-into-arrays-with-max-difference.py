42e5e080a848f3777b874851f638766637f57eeb
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2024-02-01 21:56:40 +0530

    Time: 806 ms (21.56%) | Memory: 27.1 MB (99.11%) - LeetSync

---
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def divideArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        size = len(nums)
+        if size % 3 != 0:
+            return []
+
+        nums.sort()
+
+        result = []
+        for i in range(0, size, 3):
+            if i + 2 < size and nums[i + 2] - nums[i] <= k:
+                result.append([nums[i], nums[i + 1], nums[i + 2]])
+            else:
+                return []
+        return result
