17611930f77ee265d7769bbb6110ec3dfb945ce8
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2024-02-17 23:27:06 +0530

    Time: 704 ms (13.51%) | Memory: 22.6 MB (64.86%) - LeetSync

---
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def furthestBuilding(self, h, b, l):
+        """
+        :type heights: List[int]
+        :type bricks: int
+        :type ladders: int
+        :rtype: int
+        """
+        p = []
+       
+        i = 0
+        for i in range(len(h) - 1):
+            diff = h[i + 1] - h[i]
+           
+            if diff <= 0:
+                continue
+           
+            b -= diff
+            x = heapq.heappush(p, -diff)
+            print(x)
+            if b < 0:
+                b += -heapq.heappop(p)
+                l -= 1
+               
+            if l < 0:
+                return i
+        return len(h)-1
