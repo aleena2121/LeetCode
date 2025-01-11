dee793d428e2b06aff336dcea97ca3a85b4d43c3
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2025-01-11 21:21:32 +0530

    Time: 23 ms (100.00%) | Memory: 13 MB (23.53%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -8,12 +8,10 @@ class Solution(object):
         if len(s) < k:
             return False
         s1 = set(s)
-        print(s1)
         odd = 0
         for i in s1:
             if s.count(i)%2 != 0:
                 odd+=1
-        print(odd)
         if odd > k:
             return False
         return True
