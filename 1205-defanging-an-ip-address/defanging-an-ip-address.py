4dd77f28c5b1c53193bfaf46a51c2f0c9e09a1fc
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2025-01-27 18:05:24 +0530

    Time: 14 ms (64.37%) | Memory: 12.2 MB (78.95%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        res = ""
+        for i in address:
+            if i.isnumeric():
+                res += i
+            else:
+                res += "["
+                res += i
+                res += "]"
+        
+        return res
