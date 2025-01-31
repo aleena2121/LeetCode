82675ced5cb3702c569489bc99b031d847aa8ece
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2025-01-31 09:49:21 +0530

    Time: 74 ms (12.26%) | Memory: 12.6 MB (8.05%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findLucky(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        ar = set(arr)
+        ans = -1
+        for i in arr:
+            if arr.count(i)==i:
+                ans = max(ans,i)
+        return ans
