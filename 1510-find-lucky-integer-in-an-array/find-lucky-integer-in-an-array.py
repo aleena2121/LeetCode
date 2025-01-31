65f105767482f48cd42f899c45e836dba89ded08
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2025-02-01 00:48:02 +0530

    Time: 11 ms (27.59%) | Memory: 12.5 MB (29.12%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         ar = set(arr)
         ans = -1
-        for i in arr:
+        for i in ar:
             if arr.count(i)==i:
                 ans = max(ans,i)
         return ans
