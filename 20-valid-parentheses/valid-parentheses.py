0e1f78940df1ee7557bcea7674f1f9b9d1a1e1db
aleena2121 <104380474+aleena2121@users.noreply.github.com>
2026-01-07 22:08:17 +0530

    Time: 4 ms (13.75%) | Memory: 19.3 MB (10.79%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i in "([{": 
+                stack.append(i)
+            elif i == ")":
+                if not stack or stack.pop() != "(":
+                    return False  
+            elif i == "]":
+                if not stack or stack.pop() != "[":
+                    return False 
+            elif i == "}":
+                if not stack or stack.pop() != "{":
+                    return False  
+
+        return not stack
